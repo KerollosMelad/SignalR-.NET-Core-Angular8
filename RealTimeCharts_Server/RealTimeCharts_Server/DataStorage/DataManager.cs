be6d0461175737445638e8690a497563115ef8d7
Kerollos Fahmy <Kerollos.Fahmy@integrant.com>
2020-09-14 14:40:15 +0200

    add .net core as server

---
+++ RealTimeCharts_Server/RealTimeCharts_Server/DataStorage/DataManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RealTimeCharts_Server.Models;
+
+namespace RealTimeCharts_Server.DataStorage
+{
+    public static class DataManager
+    {
+        public static List<ChartModel> GetData()
+        {
+            var r = new Random();
+            return new List<ChartModel>()
+            {
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data1" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data2" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data3" },
+                new ChartModel { Data = new List<int> { r.Next(1, 40) }, Label = "Data4" }
+            };
+        }
+    }
+}
---
+++ RealTimeCharts_Server/RealTimeCharts_Server/HubConfig/ChartHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using RealTimeCharts_Server.Models;
+
+namespace RealTimeCharts_Server.HubConfig
+{
+    
+    public class ChartHub: Hub
+    {
+        //This BroadcastChartData method will receive the message from the client
+        //and then broadcast that same message to all the clients
+        //that listen on the bradcastchratdata event.
+        [HubMethodName("broadcastchartdata")]
+        public async Task broadcastchartdata(List<ChartModel> data)
+        {
+            await Clients.All.SendAsync("broadcastchartdata", data);
+        }
+        public async Task broadcastdata(int data)
+        {
+            await Clients.All.SendAsync("broadcastdata", data);
+        }
+        
+    }
+}
